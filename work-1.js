a12404f0738db9aac0afef0dac214c50fa4c443e
Ryuhwa <im706374@gmail.com>
2024-06-12 10:44:56 +0900

    work-1

---
+++ work-1.js
@@ -0,0 +1,31 @@
+// const basicData = {
+//   name : "",
+//   age: 0,
+//   job : ""
+// }
+
+// const component = () => {
+//   const basicData = {
+//     name : "하밀",
+//     age : 900,
+//     job : "화화"
+//   }
+
+  const basicData = () => {
+    return {
+    name : "하밀",
+    age : 900,
+    job : "화화"
+    }
+  }
+
+  const component = () => {
+    let obj = basicData();
+
+    const decision = () => {};
+    
+    return `<html></html>`
+  };
+
+  
+  
